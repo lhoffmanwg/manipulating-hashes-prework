a0859470bb4a6ac8a9d9510742b7cf3e6baf5b09
Laura Hoffman <lhoffmanwg@gmail.com>
2019-03-08 04:23:52 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
   
   contacts["Freddy Mercury"][:favorite_icecream_flavors]
-          .delete do |flavor = "strawberry"|
+          .delete("strawberry")
   contacts
     
 end
